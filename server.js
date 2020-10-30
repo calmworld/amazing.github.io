0d9dfe15d7beeaa0237c3b44c820dc2fa8da4b9c
calmWorld <sarah.alhuriz@hotmail.com>
2020-10-30 11:30:19 -0600

    adding users-controllers

--- server.js
+++ server.js
@@ -6,33 +6,42 @@ const app = express();
 
 const mongoose = require('mongoose'); //include the mongoose package
 
-const productsController = require('./controllers/products.js'); //require controllers/routs from products.js
+const session = require('express-session');
+
 
 //===========================
 // Port
 //===========================
-const port = 3000;
+const port = process.env.PORT
 
 
 //==============================
 // Global Configuration
 //==============================
-const mongoURI = 'mongodb://localhost:27017/amazing' //configure mongoURI with db called amazing
+//const mongoURI = 'mongodb://localhost:27017/amazing' //configure mongoURI with db called amazing
 
 //configure mongoose Promise: mongoose.Promise = global.Promise //Optional - may or may not get a warning in terminal about this
+//===============================
+const mongodbURI = process.env.MONGODBURI
+
+require('dotenv').config()
+
+const db = mongoose.connection
+
 
 
 //============================
 // Connect to Mongoose
 //============================
-mongoose.connect(mongoURI, { 
+mongoose.connect(mongodbURI, { 
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-mongoose.connection.once('open', () => {
-    console.log('Established Connection With Mongod');
-})
+},
+() => {
+  console.log('the connection with mongod is established at', mongodbURI)
+}
+)
 
 //===========================
 // Middleware
@@ -47,8 +56,42 @@ app.use(express.urlencoded({extended: true})); //recognize the incoming object a
 app.use(express.static(__dirname + '/public')); 
 
 
+app.use(
+    session({
+      secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
+      resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
+      saveUninitialized: false // default  more info: https://www.npmjs.com/package/express-session#resave
+    })
+)
+
+//===========================
+// ERROR
+//===========================
+// Optional, but likely helpful
+// Connection Error/Success
+// Define callback functions for various events
+db.on('error', err => console.log(err.message + ' is mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+//===========================
+// Controllers
+//===========================
+const productsController = require('./controllers/products.js'); //require controllers/routs from products.js
 app.use('/products', productsController); //use routs/controllers from products.js
 
+const usersController = require('./controllers/users_controller.js')
+app.use('/users', usersController)
+
+const sessionsController = require('./controllers/sessions_controller.js')
+app.use('/sessions', sessionsController)
+
+
+//===========================
+// Routes
+//===========================
+app.get('/', (req, res) => {
+    res.redirect('/products')
+  })
 //===========================
 // Listening
 //===========================
