d1531642e1143915f7f4bf9400c4b6b9cf83942b
calmworld <sarah.alhuriz@hotmail.com>
2020-11-02 18:03:53 -0700

    added a few more seeds

--- models/seed.js
+++ models/seed.js
@@ -1,5 +1,29 @@
 productSeed = [
     {
+        "name": "Giuseppe Zanotti Women's Rw00017 Sneakers",
+        "description": "Printed Snake double zip Low Top. Rubber sole",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/71B1I1jPK8L._AC_UX500_.jpg",
+        "price": 695.00,
+        "qty": 16,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Nobis Shelby Men\'s Parka",
+        "description": "Color/Pattern: Black Approximately 37In From Shoulder To Hem Measurement Was Taken From A Size Large And May Vary Slightly By Size Design Details: Attached Hood, Four Front Pockets, Strap Detail At Shoulders And Cuffs, Rib-Knit Inner Cuffs Zipper Closure Medium Weight Shell: 100% Polyester Lining: 100% Nylon Machine Wash Imported",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/617hjBwxoiL._AC_UX569_.jpg",
+        "price": 895.00,
+        "qty": 10,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Air Force 1 'Cactus Jack' Men Sneakers",
+        "description": "Multi-color, Muted Bronze",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/71K%2ByZIAKyL._AC_UX500_.jpg",
+        "price": 999.99,
+        "qty": 6,
+        "isReadyToSell": true
+    },
+    {
         "name": "Sony X900H 65 Inch TV: 4K Ultra HD Smart LED TV",
         "description": "All of your favorite content, displayed in rich and life-like 4K HDR. The X900H is engineered with picture quality in mind, complimented by Full Array LED, TRILUMINOS Display, and X-Tended Dynamic Range technologies contributing to an experience that accurately portrays everything you watch and play. Connect Alexa to your compatible Sony TV to stream smart camera feeds with your voice. You can also ask Alexa to play music, launch video apps, and more and see it all on your Sony TV.",
         "img": "https://images-na.ssl-images-amazon.com/images/I/613yHj0LKJL._AC_SL1200_.jpg",
@@ -24,13 +48,46 @@ productSeed = [
         "isReadyToSell": true
     },
     {
-        "name": "KitchenAid KSM180QHSD 100 Year Limited Edition Queen of Hearts Stand Mixer, Passion Red",
+        "name": "KitchenAid 100 Year Limited Edition Queen of Hearts Stand Mixer",
         "description": "Add iconic style to your kitchen with this unique, limited-edition KitchenAid stand mixer. This mixer features a modern, passion-inspired color, unique 100 year celebratory touches and a substantial 5 quart capacity that's perfect for everyday family meals. The tilt-head design allows for clear access to the bowl to add ingredients for a recipe and this mixer features 59 planetary mixing action with 59 touch points around the bowl to ensure that all ingredients are thoroughly incorporated.",
         "img": "https://images-na.ssl-images-amazon.com/images/I/81HNKALPWYL._AC_SL1500_.jpg",
         "price": 569.99,
         "qty": 0,
         "isReadyToSell": false
+    },
+    {
+        "name": "Milk-Bone Soft & Chewy",
+        "description": "Milk-Bone Soft & Chewy Beef & Filet Mignon Recipe dog snacks are delicious, tender snacks that are made with real beef. Prepared with care by the makers of Milk-Bone dog snacks, these tasty treats will give your dog the simple, genuine joy that your dog gives you every day.",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/81LJMh86gNL._AC_SL1500_.jpg",
+        "price": 15.69,
+        "qty": 20,
+        "isReadyToSell": true
+    },
+    {
+        "name": "FRAME Women's Margot Dress",
+        "description": "100% Cotton. Imported. Shell: 100% cotton. Fabric: Lightweight, non-stretch. Wash cold. Ruched mock neck. Length: 33in/84cm, from shoulder",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/614gzzvKLoL._AC_UY550_.jpg",
+        "price": 398.00,
+        "qty": 3,
+        "isReadyToSell": true
+    },
+    {
+        "name": "TAG Heuer Swiss Quartz Dress Watch",
+        "description": "100% Cotton. Imported. Shell: 100% cotton. Fabric: Lightweight, non-stretch. Wash cold. Ruched mock neck. Length: 33in/84cm, from shoulder",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/918FGYglRxL._AC_UY741_.jpg",
+        "price": 978.98,
+        "qty": 8,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Cartier Tank",
+        "description": "The Tank Francaise watch features a stainless steel case and bracelet with 26 brilliant-cut diamonds set on the case. Crown is set with a blue sapphire cabochon. Silvered beaded dial with black roman numerals. Blued steel sword-shaped hands. Battery operated quartz movement. Deployment buckle. Watch size: 30.40mm x 25.05mm. Case thickness: 6.65mm. Water resistant to 30 meters/100 feet. Push and pull crown. Watch style: luxury. Cartier Tank Francaise Women's Medium Watch W4TA0009.",
+        "img": "https://images-na.ssl-images-amazon.com/images/I/519qFM69sPL._AC_UX522_.jpg",
+        "price": 8100.00,
+        "qty": 12,
+        "isReadyToSell": true
     }
+
 ]
 
 module.exports = productSeed
