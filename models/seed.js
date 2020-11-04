76912bdcc261795d13238037156f825f2e2e42f6
calmworld <sarah.alhuriz@hotmail.com>
2020-11-04 09:28:05 -0700

    fixed show page img

--- models/seed.js
+++ models/seed.js
@@ -3,8 +3,8 @@ productSeed = [
         "name": "How To Debug",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/22367738/r/il/2deb25/2355308876/il_1588xN.2355308876_dipc.jpg",
-        "price": 25.99,
-        "qty": 20,
+        "price": 35.99,
+        "qty": 27,
         "isReadyToSell": true
     },
     {
@@ -16,34 +16,42 @@ productSeed = [
         "isReadyToSell": true
     },
     {
-        "name": "Coffee Break",
+        "name": "Git Commit",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
-        "img": "https://i.etsystatic.com/15052920/r/il/ff89b9/1326738440/il_1588xN.1326738440_51c0.jpg",
-        "price": 695.00,
-        "qty": 16,
+        "img": "https://i.etsystatic.com/19942771/r/il/47b8d7/2385983129/il_1588xN.2385983129_o3u0.jpg",
+        "price": 35.69,
+        "qty": 30,
+        "isReadyToSell": true
+    },
+    {
+        "name": "It Works",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/16615387/r/il/3eac74/2088077785/il_1588xN.2088077785_g5cv.jpg",
+        "price": 71.98,
+        "qty": 38,
         "isReadyToSell": true
     },
     {
         "name": "Debugging",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/16615387/r/il/bdd1c6/2465634714/il_1588xN.2465634714_spur.jpg",
-        "price": 895.00,
-        "qty": 10,
+        "price": 58.30,
+        "qty": 51,
         "isReadyToSell": true
     },
     {
         "name": "Muscles Display",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/16615387/r/il/c9d585/2113625261/il_1588xN.2113625261_j2pw.jpg",
-        "price": 999.99,
-        "qty": 6,
+        "price": 49.99,
+        "qty": 46,
         "isReadyToSell": true
     },
     {
         "name": "Nutrition Facts",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/16615387/r/il/fa0eef/2001927719/il_1588xN.2001927719_rero.jpg",
-        "price": 139.99,
+        "price": 39.99,
         "qty": 50,
         "isReadyToSell": true
     },
@@ -52,22 +60,30 @@ productSeed = [
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/9196086/r/il/614376/1616420809/il_1588xN.1616420809_nbyq.jpg",
         "price": 64.99,
-        "qty": 8,
+        "qty": 55,
         "isReadyToSell": true
     },
     {
-        "name": "Coder Life",
+        "name": "Dress Code",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
-        "img": "https://i.etsystatic.com/21540718/r/il/64be5d/2178862746/il_1588xN.2178862746_tu69.jpg",
-        "price": 496.95,
-        "qty": 20,
+        "img": "https://i.etsystatic.com/17975591/r/il/59c094/1957880670/il_1140xN.1957880670_gajc.jpg",
+        "price": 22.79,
+        "qty": 38,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Riding a Bike",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/22367738/r/il/0714a7/2219231714/il_1140xN.2219231714_qpx8.jpg",
+        "price": 42.29,
+        "qty": 58,
         "isReadyToSell": true
     },
     {
         "name": "To Be or Not To Be",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/24034761/r/il/b8a04b/2634446974/il_1588xN.2634446974_63cy.jpg",
-        "price": 569.99,
+        "price": 69.99,
         "qty": 0,
         "isReadyToSell": false
     },
@@ -76,34 +92,33 @@ productSeed = [
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/15052920/r/il/1396b7/1381013401/il_1588xN.1381013401_127b.jpg",
         "price": 15.69,
-        "qty": 20,
-        "isReadyToSell": true
-    },
-    {
-        "name": "Git Commit",
-        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
-        "img": "https://i.etsystatic.com/19942771/r/il/47b8d7/2385983129/il_1588xN.2385983129_o3u0.jpg",
-        "price": 398.00,
-        "qty": 3,
-        "isReadyToSell": true
-    },
-    {
-        "name": "It Works",
-        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
-        "img": "https://i.etsystatic.com/16615387/r/il/3eac74/2088077785/il_1588xN.2088077785_g5cv.jpg",
-        "price": 978.98,
-        "qty": 8,
+        "qty": 25,
         "isReadyToSell": true
     },
     {
         "name": "I Can Change The World",
         "description": "Handmade. Materials: Cotton, Ink, Tender Love",
         "img": "https://i.etsystatic.com/9196086/r/il/b98484/1569005226/il_1588xN.1569005226_nxy4.jpg",
-        "price": 8100.00,
-        "qty": 12,
+        "price": 22.54,
+        "qty": 32,
         "isReadyToSell": true
-    }
-
+    },
+    {
+        "name": "Coffee Break",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/15052920/r/il/ff89b9/1326738440/il_1588xN.1326738440_51c0.jpg",
+        "price": 45.20,
+        "qty": 36,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Coder Life",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/21540718/r/il/64be5d/2178862746/il_1588xN.2178862746_tu69.jpg",
+        "price": 46.95,
+        "qty": 35,
+        "isReadyToSell": true
+    },
 ]
 
 module.exports = productSeed
