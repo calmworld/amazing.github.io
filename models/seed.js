31fdacd9dfa7142743473361aa0051ffdc62d080
calmworld <sarah.alhuriz@hotmail.com>
2020-11-03 21:04:01 -0700

    changed shop products

--- models/seed.js
+++ models/seed.js
@@ -1,88 +1,104 @@
 productSeed = [
     {
-        "name": "Giuseppe Zanotti Women's Rw00017 Sneakers",
-        "description": "Printed Snake double zip Low Top. Rubber sole",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/71B1I1jPK8L._AC_UX500_.jpg",
+        "name": "How To Debug",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/22367738/r/il/2deb25/2355308876/il_1588xN.2355308876_dipc.jpg",
+        "price": 25.99,
+        "qty": 20,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Development Process",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/22367738/r/il/57ba67/2402909535/il_1588xN.2402909535_2ntn.jpg",
+        "price": 25.99,
+        "qty": 20,
+        "isReadyToSell": true
+    },
+    {
+        "name": "Coffee Break",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/15052920/r/il/ff89b9/1326738440/il_1588xN.1326738440_51c0.jpg",
         "price": 695.00,
         "qty": 16,
         "isReadyToSell": true
     },
     {
-        "name": "Nobis Shelby Men\'s Parka",
-        "description": "Color/Pattern: Black Approximately 37In From Shoulder To Hem Measurement Was Taken From A Size Large And May Vary Slightly By Size Design Details: Attached Hood, Four Front Pockets, Strap Detail At Shoulders And Cuffs, Rib-Knit Inner Cuffs Zipper Closure Medium Weight Shell: 100% Polyester Lining: 100% Nylon Machine Wash Imported",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/617hjBwxoiL._AC_UX569_.jpg",
+        "name": "Debugging",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/16615387/r/il/bdd1c6/2465634714/il_1588xN.2465634714_spur.jpg",
         "price": 895.00,
         "qty": 10,
         "isReadyToSell": true
     },
     {
-        "name": "Air Force 1 'Cactus Jack' Men Sneakers",
-        "description": "Multi-color, Muted Bronze",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/71K%2ByZIAKyL._AC_UX500_.jpg",
+        "name": "Muscles Display",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/16615387/r/il/c9d585/2113625261/il_1588xN.2113625261_j2pw.jpg",
         "price": 999.99,
         "qty": 6,
         "isReadyToSell": true
     },
     {
-        "name": "Sony X900H 65 Inch TV: 4K Ultra HD Smart LED TV",
-        "description": "All of your favorite content, displayed in rich and life-like 4K HDR. The X900H is engineered with picture quality in mind, complimented by Full Array LED, TRILUMINOS Display, and X-Tended Dynamic Range technologies contributing to an experience that accurately portrays everything you watch and play. Connect Alexa to your compatible Sony TV to stream smart camera feeds with your voice. You can also ask Alexa to play music, launch video apps, and more and see it all on your Sony TV.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/613yHj0LKJL._AC_SL1200_.jpg",
+        "name": "Nutrition Facts",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/16615387/r/il/fa0eef/2001927719/il_1588xN.2001927719_rero.jpg",
         "price": 139.99,
         "qty": 50,
         "isReadyToSell": true
     },
     {
-        "name": "Echo Show 8",
-        "description": "Echo Show 8 connects to Alexa to give you rich stereo sound with vivid visuals on an 8” HD screen. See on-screen lyrics with Amazon Music. Set alarms and timers. Display your favorite photos. Check weather and traffic as you head out. Catch up on news highlights and movie trailers.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/61Wl-eqwcPL._AC_SL1000_.jpg",
+        "name": "Code Blooded",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/9196086/r/il/614376/1616420809/il_1588xN.1616420809_nbyq.jpg",
         "price": 64.99,
         "qty": 8,
         "isReadyToSell": true
     },
     {
-        "name": "Nikon D3500 W/ AF-P DX NIKKOR 18-55mm f/3.5-5.6G VR Black",
-        "description": "Beautiful pictures for all. You don't need to be a photographer to know a great photo when you see one. And you don't need to be a photographer to take a great photo-you just need the D3500. It's as easy to use as a point-and-shoot, but it takes beautiful DSLR photos and videos that get noticed. It feels outstanding in your hands, sturdy and balanced with controls where you want them. It's compact, durable and versatile, ideal for travel. And it works seamlessly with compatible smartphones, making it easier than ever to share your great photos. Even if you've never picked up a DSLR camera, you can take beautiful pictures with D3500.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/71TSinb4usL._AC_SL1500_.jpg",
+        "name": "Coder Life",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/21540718/r/il/64be5d/2178862746/il_1588xN.2178862746_tu69.jpg",
         "price": 496.95,
         "qty": 20,
         "isReadyToSell": true
     },
     {
-        "name": "KitchenAid 100 Year Limited Edition Queen of Hearts Stand Mixer",
-        "description": "Add iconic style to your kitchen with this unique, limited-edition KitchenAid stand mixer. This mixer features a modern, passion-inspired color, unique 100 year celebratory touches and a substantial 5 quart capacity that's perfect for everyday family meals. The tilt-head design allows for clear access to the bowl to add ingredients for a recipe and this mixer features 59 planetary mixing action with 59 touch points around the bowl to ensure that all ingredients are thoroughly incorporated.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/81HNKALPWYL._AC_SL1500_.jpg",
+        "name": "To Be or Not To Be",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/24034761/r/il/b8a04b/2634446974/il_1588xN.2634446974_63cy.jpg",
         "price": 569.99,
         "qty": 0,
         "isReadyToSell": false
     },
     {
-        "name": "Milk-Bone Soft & Chewy",
-        "description": "Milk-Bone Soft & Chewy Beef & Filet Mignon Recipe dog snacks are delicious, tender snacks that are made with real beef. Prepared with care by the makers of Milk-Bone dog snacks, these tasty treats will give your dog the simple, genuine joy that your dog gives you every day.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/81LJMh86gNL._AC_SL1500_.jpg",
+        "name": "Incomplete Data",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/15052920/r/il/1396b7/1381013401/il_1588xN.1381013401_127b.jpg",
         "price": 15.69,
         "qty": 20,
         "isReadyToSell": true
     },
     {
-        "name": "FRAME Women's Margot Dress",
-        "description": "100% Cotton. Imported. Shell: 100% cotton. Fabric: Lightweight, non-stretch. Wash cold. Ruched mock neck. Length: 33in/84cm, from shoulder",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/614gzzvKLoL._AC_UY550_.jpg",
+        "name": "Git Commit",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/19942771/r/il/47b8d7/2385983129/il_1588xN.2385983129_o3u0.jpg",
         "price": 398.00,
         "qty": 3,
         "isReadyToSell": true
     },
     {
-        "name": "TAG Heuer Swiss Quartz Dress Watch",
-        "description": "100% Cotton. Imported. Shell: 100% cotton. Fabric: Lightweight, non-stretch. Wash cold. Ruched mock neck. Length: 33in/84cm, from shoulder",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/918FGYglRxL._AC_UY741_.jpg",
+        "name": "It Works",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/16615387/r/il/3eac74/2088077785/il_1588xN.2088077785_g5cv.jpg",
         "price": 978.98,
         "qty": 8,
         "isReadyToSell": true
     },
     {
-        "name": "Cartier Tank",
-        "description": "The Tank Francaise watch features a stainless steel case and bracelet with 26 brilliant-cut diamonds set on the case. Crown is set with a blue sapphire cabochon. Silvered beaded dial with black roman numerals. Blued steel sword-shaped hands. Battery operated quartz movement. Deployment buckle. Watch size: 30.40mm x 25.05mm. Case thickness: 6.65mm. Water resistant to 30 meters/100 feet. Push and pull crown. Watch style: luxury. Cartier Tank Francaise Women's Medium Watch W4TA0009.",
-        "img": "https://images-na.ssl-images-amazon.com/images/I/519qFM69sPL._AC_UX522_.jpg",
+        "name": "I Can Change The World",
+        "description": "Handmade. Materials: Cotton, Ink, Tender Love",
+        "img": "https://i.etsystatic.com/9196086/r/il/b98484/1569005226/il_1588xN.1569005226_nxy4.jpg",
         "price": 8100.00,
         "qty": 12,
         "isReadyToSell": true
